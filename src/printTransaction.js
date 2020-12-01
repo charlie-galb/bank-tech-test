553d3300d45a2561e32ffad2aa3f3f5709210d7b
charlie-galb <galbraith.charlie@gmail.com>
2020-12-01 16:27:48 +0000

    Add if statement to account for withdrawals

--- src/printTransaction.js
+++ src/printTransaction.js
@@ -1,9 +1,15 @@
 const { getBalanceAfterTransaction } = require("../__mocks__/deposit");
 
 function printTransaction(transaction){
-    return `${transaction.getTransactionDate()}` + " || "
-    + `${transaction.getCredit().toFixed(2)}` + " ||" + " || "
-    + `${transaction.getBalanceAfterTransaction().toFixed(2)}`
+    if (transaction.getCredit !== undefined) {
+        return `${transaction.getTransactionDate()}` + " || "
+        + `${transaction.getCredit().toFixed(2)}` + " ||" + " || "
+        + `${transaction.getBalanceAfterTransaction().toFixed(2)}`
+    } else {
+        return `${transaction.getTransactionDate()}` + " || "
+        + "|| " + `${transaction.getDebit().toFixed(2)}` + " || "
+        + `${transaction.getBalanceAfterTransaction().toFixed(2)}`
+    }
 }
 
 module.exports = printTransaction;
