8afa948198972e88fc86f77176ad04bac3787590
charlie-galb <galbraith.charlie@gmail.com>
2020-12-01 16:55:38 +0000

    Add return character to end of output

--- src/printTransaction.js
+++ src/printTransaction.js
@@ -4,11 +4,11 @@ function printTransaction(transaction){
     if (transaction.getCredit !== undefined) {
         return `${transaction.getTransactionDate()}` + " || "
         + `${transaction.getCredit().toFixed(2)}` + " ||" + " || "
-        + `${transaction.getBalanceAfterTransaction().toFixed(2)}`
+        + `${transaction.getBalanceAfterTransaction().toFixed(2)}\n`
     } else {
         return `${transaction.getTransactionDate()}` + " || "
         + "|| " + `${transaction.getDebit().toFixed(2)}` + " || "
-        + `${transaction.getBalanceAfterTransaction().toFixed(2)}`
+        + `${transaction.getBalanceAfterTransaction().toFixed(2)}\n`
     }
 }
 
