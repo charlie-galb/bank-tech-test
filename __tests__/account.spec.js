d8a0c5cb73f51e1d0d6d5f1dd8a2f90d4ad3e404
charlie-galb <galbraith.charlie@gmail.com>
2020-12-01 14:35:16 +0000

    Add tests for deposit()

--- __tests__/account.spec.js
+++ __tests__/account.spec.js
@@ -9,12 +9,22 @@ describe("Account", () => {
         })
     })
 
-    describe("makeDeposit", () => {
+    describe("deposit", () => {
         test("it alters the currentBalance according to the amount credited", () => {
             let account = new Account;
             expect(account.getCurrentBalance()).toEqual(0)
-            account.makeDeposit(10)
+            account.deposit(10)
             expect(account.getCurrentBalance()).toEqual(10)
+        })
+        test("it creates a new Deposit and pushes it to the transactionHistory array", () => {
+            let account = new Account;
+            const mockDepositMaker = jest.spyOn(account, '_makeNewDeposit');
+            const mockDeposit = {}
+            mockDepositMaker.mockReturnValue(mockDeposit)
+            account.deposit(10)
+            expect(mockDepositMaker).toHaveBeenCalledWith(0, 10)
+            expect(account.getTransactionHistory().length).toEqual(1)
+            expect(account.getTransactionHistory()[0]).toEqual(mockDeposit)
         })
     })
 })
