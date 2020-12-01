932481bad8e121c105050e612a535b61dd46e162
charlie-galb <galbraith.charlie@gmail.com>
2020-12-01 10:22:59 +0000

    Add getBalanceAfterTransaction function

--- __tests__/deposit.spec.js
+++ __tests__/deposit.spec.js
@@ -1,8 +1,15 @@
 const Deposit = require("../src/deposit.js")
 
-describe("credit", () => {
+describe("getCredit", () => {
     test("it should return the amount credited to the account by the deposit", () => {
         deposit = new Deposit(500, 10)
         expect(deposit.getCredit()).toEqual(10)
+    });
+});
+
+describe("getBalanceAfterTransaction", () => {
+    test("it should return the account balance plus the amount credited", () => {
+        deposit = new Deposit(500, 10)
+        expect(deposit.getBalanceAfterTransaction()).toEqual(510)
     });
 });
--- src/deposit.js
+++ src/deposit.js
@@ -2,11 +2,16 @@ class Deposit{
     constructor(balanceBeforeTransaction, amount){
         this.balanceBeforeTransaction = balanceBeforeTransaction;
         this.amount = amount;
-    }
+        this.balanceAfterTransaction = this.balanceBeforeTransaction + amount;
+    };
 
     getCredit(){
         if (this.type = "deposit") return this.amount;
-    }
-}
+    };
+
+    getBalanceAfterTransaction(){
+        return this.balanceAfterTransaction;
+    };
+};
 
 module.exports = Deposit;
