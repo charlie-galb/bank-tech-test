45119554d5ba1af3a24ed2a167db219059370cbd
charlie-galb <galbraith.charlie@gmail.com>
2020-12-01 14:34:51 +0000

    Abstract private methods _makeNewDeposit and _pushTransaction from deposit()

--- src/account.js
+++ src/account.js
@@ -1,15 +1,32 @@
+const Deposit = require("./deposit");
+
 class Account{
 
     constructor(){
         this.currentBalance = 0;
+        this.transactionHistory = [];
     };
 
     getCurrentBalance(){
         return this.currentBalance;
     };
 
-    makeDeposit(amount){
+    getTransactionHistory(){
+        return this.transactionHistory;
+    }
+
+    deposit(amount){
+        let deposit = this._makeNewDeposit(this.currentBalance, amount);
+       this._pushtransaction(deposit);
         this.currentBalance = this.currentBalance + amount;
+    }
+
+    _makeNewDeposit(){
+        return new Deposit;
+    }
+
+    _pushtransaction(transaction){
+        this.transactionHistory.push(transaction);
     }
 };
 
