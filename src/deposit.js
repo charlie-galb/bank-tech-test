aebb1e57b77fdff8cf8a5219bb973217eee79d59
charlie-galb <galbraith.charlie@gmail.com>
2020-12-01 09:57:46 +0000

    Rename transaction class as Deposit, remove type variable

---
+++ src/deposit.js
@@ -0,0 +1,12 @@
+class Deposit{
+    constructor(balanceBeforeTransaction, amount){
+        this.balanceBeforeTransaction = balanceBeforeTransaction;
+        this.amount = amount;
+    }
+
+    getCredit(){
+        if (this.type = "deposit") return this.amount;
+    }
+}
+
+module.exports = Deposit;
