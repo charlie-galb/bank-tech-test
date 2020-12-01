6e6827e1080fc9f6e062b15079f9ede02eea7fad
charlie-galb <galbraith.charlie@gmail.com>
2020-12-01 16:27:11 +0000

    Add test for withdrawal

--- __tests__/printTransaction.spec.js
+++ __tests__/printTransaction.spec.js
@@ -1,9 +1,15 @@
 const printTransaction = require("../src/printTransaction.js")
 const depositObject = require("../__mocks__/deposit.js")
+const withdrawalObject = require("../__mocks__/withdrawal.js")
 
-describe("printStatement", () => {
-    it("prints bank statements", () => {
+
+describe("printTransaction", () => {
+
+    it("prints an individual deposit", () => {
         expect(printTransaction(depositObject)).toEqual("04/07/2019 || 10.50 || || 10.50")
-    })
-})
+    });
 
+    it("prints an individual withdrawal", () => {
+        expect(printTransaction(withdrawalObject)).toEqual("04/07/2019 || || 10.50 || 0.00")
+    });
+})
