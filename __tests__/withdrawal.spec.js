2f346865711c2d49ed3e172310d294f21ec08db4
charlie-galb <galbraith.charlie@gmail.com>
2020-12-01 11:52:48 +0000

    Add getTransactionDate function to Withdrawal class

--- __tests__/withdrawal.spec.js
+++ __tests__/withdrawal.spec.js
@@ -10,4 +10,14 @@ describe("Withdrawal", () => {
             expect(withdrawal.getDebit()).toEqual(10)
         });
     });
+
+    describe("getTransactionDate", () => {
+        test("it should return the amount credited to the account by the deposit", () => {
+            const realNow = Date.now
+            global.Date.now = jest.fn(() => new Date('2019-04-07T10:20:30Z'))
+            let withdrawal = new Withdrawal(6, 3)
+            expect(withdrawal.getTransactionDate()).toEqual( "Sun Apr 07 2019 11:20:30 GMT+0100 (British Summer Time)")
+            global.Date.now = realNow
+        });
+    });
 })
--- src/withdrawal.js
+++ src/withdrawal.js
@@ -3,11 +3,16 @@ class Withdrawal{
     constructor(balanceBeforeTransaction, amount){
         this.balanceBeforeTransaction = balanceBeforeTransaction;
         this.debit = amount;
+        this.transactionDate = Date.now();
     }
 
     getDebit(){
         return this.debit;
-    }
-}
+    };
+
+    getTransactionDate(){
+        return this.transactionDate.toString();
+    };
+};
 
 module.exports = Withdrawal;
