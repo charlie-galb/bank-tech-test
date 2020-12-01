80cb3fb7a7b9c2d8854e7d8b518be43acb05573a
charlie-galb <galbraith.charlie@gmail.com>
2020-12-01 12:09:33 +0000

    Add Account class with constructor and gett for currentBalance

---
+++ src/account.js
@@ -0,0 +1,12 @@
+class Account{
+
+    constructor(){
+        this.currentBalance = 0;
+    };
+
+    getCurrentBalance(){
+        return this.currentBalance;
+    };
+};
+
+module.exports = Account;
