5245fd712c385ba8799cd2c7c2c331c428bf1301
charlie-galb <galbraith.charlie@gmail.com>
2020-12-02 09:01:40 +0000

    Change matchers to 'toMatch'

--- __tests__/printTransaction.spec.js
+++ __tests__/printTransaction.spec.js
@@ -6,10 +6,11 @@ const withdrawalObject = require("../__mocks__/withdrawal.js")
 describe("printTransaction", () => {
 
     it("prints an individual deposit", () => {
-        expect(printTransaction(depositObject)).toEqual("04/07/2019 || 10.50 || || 10.50")
+        console.log(printTransaction(depositObject))
+        expect(printTransaction(depositObject)).toMatch("04/07/2019 || 10.50 || || 10.50")
     });
 
     it("prints an individual withdrawal", () => {
-        expect(printTransaction(withdrawalObject)).toEqual("04/07/2019 || || 10.50 || 0.00")
+        expect(printTransaction(withdrawalObject)).toMatch("04/07/2019 || || 10.50 || 0.00")
     });
 })
