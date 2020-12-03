868a3bb27b815675c09009d6070fe7c2ad3c8fbe
charlie-galb <galbraith.charlie@gmail.com>
2020-12-03 12:23:39 +0000

    Remove code shared with parent

--- src/withdrawal.js
+++ src/withdrawal.js
@@ -1,22 +1,13 @@
-class Withdrawal{
+const Transaction = require("./transaction.js")
 
+class Withdrawal extends Transaction{
     constructor(balanceBeforeTransaction, amount){
-        this.balanceBeforeTransaction = balanceBeforeTransaction;
-        this.debit = amount;
-        this.transactionDate = Date.now();
+        super(balanceBeforeTransaction, amount);
         this.balanceAfterTransaction = this.balanceBeforeTransaction - amount;
     }
-
+    
     getDebit(){
-        return this.debit;
-    }
-
-    getTransactionDate(){
-        return new Date(this.transactionDate).toLocaleString().split(',')[0];
-    }
-
-    getBalanceAfterTransaction(){
-        return this.balanceAfterTransaction;
+        return this.amount;
     }
 }
 
