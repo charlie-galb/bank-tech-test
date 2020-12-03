e816027783171ea7c8859e5080f38dfdfc51bb5d
charlie-galb <galbraith.charlie@gmail.com>
2020-12-03 12:06:26 +0000

    Moved printTransaction inside PrintStatement function

--- src/printStatement.js
+++ src/printStatement.js
@@ -1,7 +1,18 @@
-function printStatement(transactionArray){
+function printStatement(transactionArray) {
     let heading = "| date || credit || debit || balance\n";
     let transactions = "";
     let reverseChronTransactionArray = transactionArray.reverse()
+    let printTransaction = function(transaction) {
+        if (transaction.getCredit !== undefined) {
+            return `${transaction.getTransactionDate()}` + " || "
+            + `${transaction.getCredit().toFixed(2)}` + " ||" + " || "
+            + `${transaction.getBalanceAfterTransaction().toFixed(2)}\n`
+        } else {
+            return `${transaction.getTransactionDate()}` + " || "
+            + "|| " + `${transaction.getDebit().toFixed(2)}` + " || "
+            + `${transaction.getBalanceAfterTransaction().toFixed(2)}\n`
+        }
+    }
     
     for(let i = 0; i < reverseChronTransactionArray.length; i++) {
        transactions += printTransaction(reverseChronTransactionArray[i])
@@ -11,16 +22,16 @@ function printStatement(transactionArray){
     return statement;
 }
 
-function printTransaction(transaction){
-    if (transaction.getCredit !== undefined) {
-        return `${transaction.getTransactionDate()}` + " || "
-        + `${transaction.getCredit().toFixed(2)}` + " ||" + " || "
-        + `${transaction.getBalanceAfterTransaction().toFixed(2)}\n`
-    } else {
-        return `${transaction.getTransactionDate()}` + " || "
-        + "|| " + `${transaction.getDebit().toFixed(2)}` + " || "
-        + `${transaction.getBalanceAfterTransaction().toFixed(2)}\n`
-    }
-}
+// function printTransaction(transaction){
+//     if (transaction.getCredit !== undefined) {
+//         return `${transaction.getTransactionDate()}` + " || "
+//         + `${transaction.getCredit().toFixed(2)}` + " ||" + " || "
+//         + `${transaction.getBalanceAfterTransaction().toFixed(2)}\n`
+//     } else {
+//         return `${transaction.getTransactionDate()}` + " || "
+//         + "|| " + `${transaction.getDebit().toFixed(2)}` + " || "
+//         + `${transaction.getBalanceAfterTransaction().toFixed(2)}\n`
+//     }
+// }
 
 module.exports = printStatement;
