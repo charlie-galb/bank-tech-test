adc9e6e9a6543fdfb66b88d2efc2704cb0f965ed
charlie-galb <galbraith.charlie@gmail.com>
2020-12-03 11:43:20 +0000

    Move transactions mocks into the file for readability

--- __tests__/printStatement.spec.js
+++ __tests__/printStatement.spec.js
@@ -2,11 +2,21 @@ const printStatement = require("../src/printStatement.js")
 const depositObject = require("../__mocks__/deposit.js")
 const withdrawalObject = require("../__mocks__/withdrawal.js")
 
-const transactionArray = [depositObject, withdrawalObject]
+const withdrawalMock = {
+    getDebit: () => 10.5,
+    getBalanceAfterTransaction: () => 0,
+  getTransactionDate: () => "04/07/2019"
+}
+const depositMock = {
+    getCredit: () => 10.5,
+    getBalanceAfterTransaction: () => 10.5,
+  getTransactionDate: () => "04/07/2019"
+}
+const mockTransactionHistory = [depositMock, withdrawalMock]
 
 describe("printStatement", () => {
     it("prints bank statements", () => {
-        expect(printStatement(transactionArray)).toEqual(
-            "| date || credit || debit || balance\n04/07/2019 || 10.50 || || 10.50\n04/07/2019 || || 10.50 || 0.00\n")
+        expect(printStatement(mockTransactionHistory)).toEqual(
+            "| date || credit || debit || balance\n04/07/2019 || || 10.50 || 0.00\n04/07/2019 || 10.50 || || 10.50\n")
     })
 })
