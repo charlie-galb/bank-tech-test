c3f58f3310b7f7936dd7ac4b8ae75b5e12cbde88
charlie-galb <galbraith.charlie@gmail.com>
2020-12-03 11:48:32 +0000

    Move printTransaction function into this file

--- src/printStatement.js
+++ src/printStatement.js
@@ -1,15 +1,26 @@
-const printTransaction = require("./printTransaction.js")
-
 function printStatement(transactionArray){
     let heading = "| date || credit || debit || balance\n";
     let transactions = "";
+    let reverseChronTransactionArray = transactionArray.reverse()
     
-    for(let i = 0; i < transactionArray.length; i++) {
-       transactions += printTransaction(transactionArray[i])
+    for(let i = 0; i < reverseChronTransactionArray.length; i++) {
+       transactions += printTransaction(reverseChronTransactionArray[i])
     }
     let statement = heading + transactions;
     console.log(statement);
     return statement;
+}
+
+function printTransaction(transaction){
+    if (transaction.getCredit !== undefined) {
+        return `${transaction.getTransactionDate()}` + " || "
+        + `${transaction.getCredit().toFixed(2)}` + " ||" + " || "
+        + `${transaction.getBalanceAfterTransaction().toFixed(2)}\n`
+    } else {
+        return `${transaction.getTransactionDate()}` + " || "
+        + "|| " + `${transaction.getDebit().toFixed(2)}` + " || "
+        + `${transaction.getBalanceAfterTransaction().toFixed(2)}\n`
+    }
 }
 
 module.exports = printStatement;
